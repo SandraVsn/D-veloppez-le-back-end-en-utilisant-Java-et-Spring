67bec36f34a0d294b22ccc70c4240e4ad780a73c
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-12-08 16:05:47 +0100

    fix: add swagger config for title & jwt support

---
+++ src/main/java/com/openclassrooms/chatop/configuration/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.chatop.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+            .info(new Info().title("Chatop API")
+                .description("You will find here all the routes available for the Chatop API")
+                .version("1.0").contact(new Contact().name("Sandra VOISIN").url("https://github.com/SandraVsn")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer");
+    }
+
+}
